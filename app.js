2f81e7b116be980b3d135844bb0bc8130d6bec29
Canberk <47719706+LaniakeaCode@users.noreply.github.com>
2020-11-15 16:00:53 +0300

    proje-1

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
 var db = mongojs('items', ['items']);
+var db2 = mongojs('items',['items2']);
 var bodyParser = require("body-parser");
 
 app.use(bodyParser.json({limit: "50mb"}));
@@ -9,9 +10,10 @@ app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:500
 app.use(express.static(__dirname + '/'));
 app.use(bodyParser.json());
 
+
 var port = 3000;
 
-app.get('/items', function (req, res) {
+app.get('/items6', function (req, res) {
     console.log("veri al");
     db.items.find(function (err, docs) {
         // console.log(docs);
@@ -19,7 +21,14 @@ app.get('/items', function (req, res) {
     })
 })
 
-app.post('/items', function (req, res) {
+app.get('/items16', function(req, res) {
+    db2.items2.find(function(err, docs) {
+        res.json(docs);
+        // its for login
+    })
+})
+
+app.post('/items6', function (req, res) {
     console.log("veri ekle");
     // console.log(req.body);
     db.items.insert(req.body, function (err, doc) {
@@ -27,7 +36,16 @@ app.post('/items', function (req, res) {
     })
 })
 
-app.delete('/items/:id', function (req, res) {
+app.post('/items16', function(req,res){
+    console.log("kullanıcı veririsi ekle");
+    db2.items2.insert(req.body, function ( err, doc) {
+        res.json(doc);
+        // its  for login 
+    })
+})
+
+
+app.delete('/items6/:id', function (req, res) {
     console.log("silindi");
     var id = req.params.id;
     
@@ -35,19 +53,49 @@ app.delete('/items/:id', function (req, res) {
 
     db.items.remove({_id: mongojs.ObjectId(id) }, function (err, doc) {
         res.json(doc);
-    });
+    })
 })
 
-app.get('/items/:id', function (req, res) {
+app.delete('/items16/:id', function (req, res) {
+    console.log("kullanıcı hesabı silindi");
+    var id = req.params.id;
+
+    console.log(id);
+
+    db2.items2.remove({_id: mongojs.ObjectId(id)}, function (err, doc){ //its for login
+        res.json(doc);
+    })
+})
+
+app.get('/items6/:id', function (req, res) {
     var id = req.params.id;
     console.log(id);
     
     db.items.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
       res.json(doc);
-    });
-  });
+    })
+  })
 
-app.put('/items/:id', function(req, res){
+app.get('/items16/:id', function (req, res) {
+    var id = req.params.id;
+    console.log(id);
+
+    db2.items2.findOne({_id: mongojs.ObjectId(id)}, function(err, doc) { // its for login
+        res.json(doc);
+    })
+})
+
+app.get('/items16/:id', function (req, res) {
+    var id = req.params.id;
+    console.log(id);
+
+    db2.items2.findOne({_id: mongojs.ObjectId(id)}, function(err, doc) { // its for login
+        res.json(doc);
+    })
+})
+
+
+app.put('/items6/:id', function(req, res){
     var id = req.params.id;
     // console.log(req.body.url + " ******");
     console.log("değiştir");
@@ -64,6 +112,24 @@ app.put('/items/:id', function(req, res){
         
     })
 })
+
+app.put('/items16/:id', function(req, res){
+    var id = req.params.id;                                     // its for login
+    console.log("kullanıcı bilgilerini değiştir");
+    db2.items2.findAndModify({
+        query: {_id:mongojs.ObjectId(id)},
+        update: {$set: {
+            username: req.body.username,
+            password: req.body.password,
+            email: req.body.email}},
+            new: true},function(err, doc) {
+                res.json(doc);
+            
+      
+    })
+})
+
+
 app.listen(port, () => {
     console.log('Listening on ' + port);
 });
--- controller.js
+++ controller.js
@@ -1,67 +1,115 @@
-var myApp = angular.module('myApp',[]);
+var myApp = angular.module('myApp', []);
 
-myApp.controller('myAppController',function($scope,$http){
+myApp.controller('myAppController', function ($scope, $http) {
 
-    $scope.poster = null; 
+    $scope.poster = null;
 
     $scope.getItem = function () {
-        $http.get("/items").then(function (res) {
+        $http.get("/items6").then(function (res) {
             // console.log(res);
             console.log("Valar Dohaeris");
             $scope.items = res.data;
         })
-        
+
     }
+
+
+    $scope.showItemInBeginning = function () {
+
+        console.log("Valar Morghulis");
+        $scope.getItem();
+    }
+
+    $scope.getItemOfuser = function () {
+        $http.get("/items16").then(function (res) {        // its for login's controller
+            console.log("kullanıcı bilgileri alındı")
+            $scope.items = res.data;
+        })
+    }
+
     $scope.addItem = function () {
         $scope.item.url = $scope.poster;
-        $http.post("/items",$scope.item).then(function (res){
+        $http.post("/items6", $scope.item).then(function (res) {
             // console.log(res);
             console.log("Valar Morghulis");
             $scope.getItem();
         })
     }
 
-    $scope.removeItem = function(item){
+    $scope.addItemOfuser = function () {                     // its for login's controller
+        $http.post("/items16", $scope.item).then(function (res) {
+            console.log("kullanıcı bilgileri eklendi");
+            $scope.getItemOfuser();
+        })
+    }
+
+    $scope.removeItem = function (item) {
         console.log(item);
-        $http.delete('/items/'+ item._id ).then(function (res){
+        $http.delete('/items6/' + item._id).then(function (res) {
             console.log(res);
             $scope.getItem();
         })
     }
 
-    $scope.eddie= function(item){
+    $scope.removeItemOfuser = function (item) {             // its for login's controller
         console.log(item);
-        $http.get('/items/' +item._id).then(function(res) {
+        $http.delete('/items16/' + item._id).then(function (res) {
+            console.log(res);
+            $scope.getItemOfuser();
+        })
+    }
+
+    $scope.eddie = function (item) {
+        console.log(item);
+        $http.get('/items6/' + item._id).then(function (res) {
             $scope.item = res.data;
         });
-    };
+    }
 
-    $scope.dating = function() {
+    $scope.selectItemOfuser = function (item) {             // its for login's controller
+        console.log(item);
+        $http.get('/items16/' + item._id).then(function (res) {
+            $scope.item = res.data;
+        })
+    }
+
+    $scope.dating = function () {
         $scope.item.url = $scope.poster;
         console.log($scope.item._id);
-        $http.put('/items/' + $scope.item._id,$scope.item).then(function(res){
+        $http.put('/items6/' + $scope.item._id, $scope.item).then(function (res) {
             console.log(res);
             $scope.getItem();
         })
-    };
+    }
+
+    $scope.updatingItemOfuser = function () {         // its for login
+        console.log($scope.item._id);
+        $http.put('/items16' + $scope.item._id, $scope.item).then(function (res) {
+            console.log(res);
+            $scope.getItemOfuser();
+        })
+
+    }
+
+
 
     $scope.fileNameChanged = function (item) {
         let file = item.files[0];
-      
-        let reader = new FileReader();      
-      
-        reader.onload = function(e) {
-        //   console.log(e.target.result);
-          $scope.poster = e.target.result;
 
-        //   document.getElementById("blah").src = e.target.result
+        let reader = new FileReader();
+
+        reader.onload = function (e) {
+            //   console.log(e.target.result);
+            $scope.poster = e.target.result;
+
+            //   document.getElementById("blah").src = e.target.result
         };
-      
-        reader.onerror = function() {
-          console.log(reader.error);
+
+        reader.onerror = function () {
+            console.log(reader.error);
         };
         reader.readAsDataURL(item.files[0]);
 
-      }
+    }
 
 })
