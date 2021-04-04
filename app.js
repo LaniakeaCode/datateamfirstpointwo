1a34cb91c5189ab2408d518a64e96b1b442604ef
Canberk <47719706+LaniakeaCode@users.noreply.github.com>
2021-04-04 23:36:32 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,12 +2,33 @@ var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
 var db = mongojs('items', ['items']);
-var db2 = mongojs('items',['items2']);
+var db2 = mongojs('items', ['items2']);
 var db3 = mongojs('items', ['hollyitems']);
 var bodyParser = require("body-parser");
+var BasicHttpBinding = require('wcf.js').BasicHttpBinding //wcf1
+    , Proxy = require('wcf.js').Proxy
+    , binding = new BasicHttpBinding(
+        {
+            SecurityMode: "TransportWithMessageCredential"
+            , MessageClientCredentialType: "UserName"
+        })
+    , proxy = new Proxy(binding, "http://localhost:7171/Service/clearUsername")
+    , message = "<Envelope xmlns='http://schemas.xmlsoap.org/soap/envelope/'>" +
+        "<Header />" +
+        "<Body>" +
+        "<GetData xmlns='http://tempuri.org/'>" +
+        "<value>123</value>" +
+        "</GetData>" +
+        "</Body>" +
+        "</Envelope>"
+proxy.ClientCredentials.Username.Username = "yaron"
+proxy.ClientCredentials.Username.Password = "1234"
+proxy.send(message, "http://tempuri.org/IService/GetData", function (response, ctx) {
+    console.log(response)
+});//wcf2
 
-app.use(bodyParser.json({limit: "50mb"}));
-app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
 app.use(express.static(__dirname + '/'));
 app.use(bodyParser.json());
 
@@ -22,16 +43,16 @@ app.get('/items6', function (req, res) { // filmeri getirir
     })
 })
 
-app.get('/items16', function(req, res) {
-    db2.items2.find(function(err, docs) {
+app.get('/items16', function (req, res) {
+    db2.items2.find(function (err, docs) {
         res.json(docs);
         // its for login  kullanıcı bilgilerini getirir
     })
 })
 
 
-app.get('/magza', function(req, res) {
-    db3.hollyitems.find(function(err, docs) {
+app.get('/magza', function (req, res) {
+    db3.hollyitems.find(function (err, docs) {
         res.json(docs); // mağza için ürünleri getirir
     })
 })
@@ -44,27 +65,27 @@ app.post('/items6', function (req, res) {
     })
 })
 
-app.post('/items16', function(req,res){
+app.post('/items16', function (req, res) {
     console.log("kullanıcı veririsi ekle");
-    db2.items2.insert(req.body, function ( err, doc) {
+    db2.items2.insert(req.body, function (err, doc) {
         res.json(doc);
         // its  for login   kullanıcı bilgileri eklenebillir controller js ile bağlantılı olarak formdaki bilgileri alır
     })
 })
 
-app.post('/magza', function(req,res){
+app.post('/magza', function (req, res) {
     console.log("kullanıcı verisi ekle");
-    db3.hollyitems.insert(req.body, function( err, doc) {
+    db3.hollyitems.insert(req.body, function (err, doc) {
         res.json(doc);
     })
 })
 
 //Başka Fikir: ayrıca kendi ürün tasarımlarınıda ekleyebilirler
 
-app.post('/magza/:id' , function (req, res) { // item kopyalayıp ekleme lazım 
+app.post('/magza/:id', function (req, res) { // item kopyalayıp ekleme lazım 
     var id = req.params.id;
     console.log("item kopyalandı ve değiştirildi");  // ürünlerin idsi seçilip o ürünün kopyasını mağza veritabanına ekler.
-    var user = findOne({_id: mongojs.ObjectId(id)}, function(err, doc) { // 
+    var user = findOne({ _id: mongojs.ObjectId(id) }, function (err, doc) { // 
         res.json(doc);  // its 
     })
 
@@ -73,7 +94,7 @@ app.post('/magza/:id' , function (req, res) { // item kopyalayıp ekleme lazım
     user.adet = req.params.adet;
     user.stoksayısı = req.params.stoksayısı;
 
-    db3.hollyitems.insert(user, function ( err, doc){ // burda bi şekilde items2 'deki ki ürünü seçen kullanıcının sipariş listesine bu id'yi eklemem lazım.
+    db3.hollyitems.insert(user, function (err, doc) { // burda bi şekilde items2 'deki ki ürünü seçen kullanıcının sipariş listesine bu id'yi eklemem lazım.
         res.json(doc); // error olursa 
     })//19dec not usefull in front-end
 
@@ -83,10 +104,10 @@ app.post('/magza/:id' , function (req, res) { // item kopyalayıp ekleme lazım
 app.delete('/items6/:id', function (req, res) { // formda seçilen filmi silme
     console.log("silindi");
     var id = req.params.id;
-    
+
     console.log(id);
 
-    db.items.remove({_id: mongojs.ObjectId(id) }, function (err, doc) {
+    db.items.remove({ _id: mongojs.ObjectId(id) }, function (err, doc) {
         res.json(doc);
     })
 })
@@ -97,7 +118,7 @@ app.delete('/items16/:id', function (req, res) { // formda seçilen kullanıcıy
 
     console.log(id);
 
-    db2.items2.remove({_id: mongojs.ObjectId(id)}, function (err, doc){ //its for login
+    db2.items2.remove({ _id: mongojs.ObjectId(id) }, function (err, doc) { //its for login
         res.json(doc);
     })
 })
@@ -108,7 +129,7 @@ app.delete('/magza/:id', function (req, res) {
 
     console.log(id);
 
-    db3.hollyitems.remove({_id: mongojs.ObjectId(id)}, function(err, doc){
+    db3.hollyitems.remove({ _id: mongojs.ObjectId(id) }, function (err, doc) {
         res.json(doc);
     })
 })
@@ -116,17 +137,17 @@ app.delete('/magza/:id', function (req, res) {
 app.get('/items6/:id', function (req, res) { // formda seçilen filmi bulup getirme
     var id = req.params.id;
     console.log(id);
-    
-    db.items.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
-      res.json(doc);
+
+    db.items.findOne({ _id: mongojs.ObjectId(id) }, function (err, doc) {
+        res.json(doc);
     })
-  })
+})
 
 app.get('/items16/:id', function (req, res) { // formda seçilen kullanıcı bilgilerini silme
     var id = req.params.id;
     console.log(id);
 
-    db2.items2.findOne({_id: mongojs.ObjectId(id)}, function(err, doc) { // its for login
+    db2.items2.findOne({ _id: mongojs.ObjectId(id) }, function (err, doc) { // its for login
         res.json(doc);
     })
 })
@@ -134,54 +155,60 @@ app.get('/items16/:id', function (req, res) { // formda seçilen kullanıcı bil
 app.get('/magza/:id', function (req, res) { // mağza için id'ye göre veritabanından item getirir.
     var id = req.params.id;
     console.log(id);
-    
-    db3.hollyitems.findOne({_id: mongojs.ObjectId(id)}, function(err, doc) {
+
+    db3.hollyitems.findOne({ _id: mongojs.ObjectId(id) }, function (err, doc) {
         res.json(doc);
     })
 
 
 })
 
-app.put('/items6/:id', function(req, res){ // seçilen filmin veritabananında update edilmesi
+app.put('/items6/:id', function (req, res) { // seçilen filmin veritabananında update edilmesi
     var id = req.params.id;
     // console.log(req.body.url + " ******");
     console.log("değiştir");
     db.items.findAndModify({
-        query: {_id: mongojs.ObjectId(id)},
-        update: {$set: {
-            url: req.body.url,
-            name: req.body.name,
-            year: req.body.year,
-            IMDb: req.body.IMDb}},
-            new: true},function(err, doc) {
-                res.json(doc);
-            
-        
+        query: { _id: mongojs.ObjectId(id) },
+        update: {
+            $set: {
+                url: req.body.url,
+                name: req.body.name,
+                year: req.body.year,
+                IMDb: req.body.IMDb
+            }
+        },
+        new: true
+    }, function (err, doc) {
+        res.json(doc);
+
+
     })
 })
 
-app.put('/magza/:id', function(req, res){
+app.put('/magza/:id', function (req, res) {
     var id = req.params.id;
     console.log("değiştir");
     db3.items.findAndModify({
-        query: {_id: mongojs.ObjectId(id)},
-        update: {$set: {
-            url: req.body.url,
-            size: req.body.size,
-            adet: req.body.adet,
-            stoksayısı = req.body.stoksayısı
-        }}
-        
+        query: { _id: mongojs.ObjectId(id) },
+        update: {
+            $set: {
+                url: req.body.url,
+                size: req.body.size,
+                adet: req.body.adet,
+                // stoksayısı = req.body.stoksayısı
+            }
+        }
+
     })
 })
 
 var siparisler = [];
-app.put('/items16/:id', function(req, res){
+app.put('/items16/:id', function (req, res) {
     var id = req.params.id;                                     // seçilen kullanıcı bilgilerinin veritabanından değiştirilmesi
     console.log("kullanıcı bilgilerini değiştir");
     //burdan
     var id2 = req.params.id2;
-    var user = findOne({_id: mongojs.ObjectId(id2)}, function(err, doc) { // 
+    var user = findOne({ _id: mongojs.ObjectId(id2) }, function (err, doc) { // 
         res.json(doc);  // its 
     })
 
@@ -190,18 +217,22 @@ app.put('/items16/:id', function(req, res){
     user.adet = req.params.adet;
     user.stoksayısı = req.params.stoksayısı;
     siparisler.push(user_id);
-//buraya kadar sipariş ekleme listeye
+    //buraya kadar sipariş ekleme listeye
     db2.items2.findAndModify({
-        query: {_id:mongojs.ObjectId(id)},
-        update: {$set: {
-            username: req.body.username,
-            password: req.body.password,
-            email: req.body.email,
-            siparisler: siparisler}},
-            new: true},function(err, doc) {
-                res.json(doc);
-            
-      
+        query: { _id: mongojs.ObjectId(id) },
+        update: {
+            $set: {
+                username: req.body.username,
+                password: req.body.password,
+                email: req.body.email,
+                siparisler: siparisler
+            }
+        },
+        new: true
+    }, function (err, doc) {
+        res.json(doc);
+
+
     })
 })
 
